b09b46ad5affdca2ac2b22458b575f19b028574f
kulagowskim <matkulagowski@gmail.com>
2022-08-29 14:40:08 +0200

    test

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 (async function () {
-  const [_, __, arg3, arg4] = process.argv;
+  const [_, __, arg3, arg4, arg5, arg6] = process.argv;
   console.log(`npx node_modules/${arg3} 
   ${arg4}`);
+  console.log(arg5);
+  console.log(arg6);
 })();
